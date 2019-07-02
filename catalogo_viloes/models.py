a27e959a6d2e3512a74ba1bcb3d65c96942fa0f1
matheus <matheusmsantos15@gmail.com>
2019-07-02 16:30:07 -0300

    criar funcao str

--- catalogo_viloes/models.py
+++ catalogo_viloes/models.py
@@ -34,3 +34,7 @@ class Vilao(models.Model):
     nivel_maldade = models.PositiveIntegerField(choices=nivel_valor)
     vivo = models.BooleanField(default=True)
     foto = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.nome
+
